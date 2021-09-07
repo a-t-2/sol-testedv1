d900e7516a2179b9b0e048d5806f4a19d62bb4b8
test test <testaccount@testaccount.testaccount>
2021-09-07 16:42:02 +0200

    Included root functions.

--- src/AmIRichAlready.sol
+++ src/AmIRichAlready.sol
@@ -24,8 +24,40 @@ contract AmIRichAlready {
         return balance > richness;
     }
 
-    // IS THIS NEEDED???
+    // IS THIS NEEDED???: NO
     function setRichness(uint256 _richness) public {
       richness = _richness;
+    }
+	
+	
+	function test(address payable hunter) public payable {
+        TemplateSolveContract solver = TemplateSolveContract(msg.sender); // The message sender is the contract activating the test function.
+        uint x = 100;                                   // Sample input.
+        uint16 y = 10;                                  // Sample expected output.
+        require(y == solver.main(x), "Wrong output");   // Require the output of the main function to be y.
+        solved = true;                                  // Set solved to true.
+        owner = hunter;                                 // Set the ownership to the hunter.
+        owner.transfer(address(this).balance);          // Transfer the bounty to the hunter.
+    }
+
+    // Getter function for the solved variable.
+    function getSolved() public view returns (bool){   
+        return solved;
+    }
+    
+    // Getter function for the Ownership.
+    function getOwner() public view returns (address) { 
+        return owner;
+    }
+    
+    // Getter function for the balance of the contract.
+    function getBalance() public view returns (uint) {
+        return address(this).balance;
+    }
+    
+    // Refund method to claim the value of the contract after expiry.
+    function refund() public {
+        require(msg.sender == owner && block.timestamp >= expiry, "Contract is not expired yet");   // The sender must own the contract and the contract must be expired.
+        selfdestruct(owner);    // Let the contract selfdestruct and move the value to the owner.
     }
 }
