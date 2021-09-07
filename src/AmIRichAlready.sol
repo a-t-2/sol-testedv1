91f7c08fb6f7ac413547ae36ad27ab0a7472eb4c
test test <testaccount@testaccount.testaccount>
2021-09-07 16:40:32 +0200

    Included askroot variables.

--- src/AmIRichAlready.sol
+++ src/AmIRichAlready.sol
@@ -5,8 +5,15 @@ interface IERC20 {
 }
 
 contract AmIRichAlready {
+
+	// Declare original variables
     IERC20 private tokenContract;
     uint public richness = 1000000 * 10 ** 18;
+	
+	// Declare root contract variables
+	bool solved;    // Boolean to denote if contract is solved.
+    address payable owner;  // Owner of the contract, first this is the sponser.
+    uint expiry;        // Get the time when the contract expires.
 
     constructor (IERC20 _tokenContract) public {
         tokenContract = _tokenContract;
